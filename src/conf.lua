5db881a004d66e5c35693f11caa30e323873a38e
Chitkul Lakshya <consolemaster@gmail.com>
2026-02-25 10:00:00 +0000

    chore(desktop): initialize LÖVE conf.lua and main boot script

---
+++ src/conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.window.title = "ZYNC Desktop"
+    t.window.width = 1024
+    t.window.height = 768
+end
---
+++ src/main.lua
@@ -0,0 +1,6 @@
+function love.load()
+    print("ZYNC Desktop Loaded")
+end
+function love.draw()
+    love.graphics.print("Welcome to ZYNC Desktop", 400, 300)
+end
